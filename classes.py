230c4c9390f69a0e23f02d4c988b6bc9224cc126
Qdavis24 <quoncdavis@gmail.com>
2025-03-01 16:52:41 -0700

    add images for pieces add handler class Game to orchastrate use of other objects (graph, position, and piece objects) create display Game status bar for ui finalizing selection and movement inside handler finalize pawn specific valid movement logic

--- classes.py
+++ classes.py
@@ -5,7 +5,9 @@ class Position:
     COLUMNS = 8
     ROWS = 8
 
-    def __init__(self, width, height):
+    def __init__(self, start_x, start_y, width, height):
+        self.start_x = start_x
+        self.start_y = start_y
         self.width = width
         self.height = height
         self.piece_width = None
@@ -27,12 +29,12 @@ class Position:
         self.offset_y = self.piece_height / 2
 
     def _create_dimension_bounds(self):
-        self.row_bounds = [self.piece_height * i for i in range(self.ROWS + 1)]
-        self.col_bounds = [self.piece_width * i for i in range(self.COLUMNS + 1)]
+        self.row_bounds = [self.piece_height * i + self.start_y for i in range(self.ROWS + 1)]
+        self.col_bounds = [self.piece_width * i + self.start_x for i in range(self.COLUMNS + 1)]
 
     def _create_position_matrix(self):
 
-        curr_pos = [self.offset_x, self.offset_y]
+        curr_pos = [self.offset_x + self.start_x, self.offset_y + self.start_y]
 
         for i in range(self.ROWS):
             column = []
@@ -40,30 +42,52 @@ class Position:
                 column.append(tuple(curr_pos))
                 curr_pos[0] += self.piece_width
             self.position_matrix.append(column)
-            curr_pos[0] = self.offset_x
+            curr_pos[0] = self.offset_x + self.start_x
             curr_pos[1] += self.piece_height
 
 
-class Events:
+class Game:
     def __init__(self, position, graph):
         self.position = position
         self.graph = graph
-        self.select = True
         self.curr_node = None
+        self.curr_player = "white"
 
     def handle_click(self, click_pos):
-        if self.select:
-            self.curr_node = self._get_node_from_click(click_pos)
-            self.select = False
-        else:
-            to = self._get_node_from_click(click_pos)
-            valid_to_nodes = self.curr_node.piece.valid_moves(self.curr_node)
-            if to in valid_to_nodes:
-                self.curr_node.piece.update(to.position)
-                self.graph.update_node(self.curr_node, to)
+        print(f"{self.curr_player} | {self.curr_node}")
+        clicked_node = self._get_node_from_click(click_pos)
+        type_click = self._type_click(clicked_node)
+        print(f"type : {type_click}")
+        if type_click == "null":
+            return
+        elif type_click == "select":
+            self._select_click(clicked_node)
+        elif type_click == "move":
+            to_node = self._get_node_from_click(click_pos)
+            self._move_click(to_node)
 
-                self.select = True
-                self.curr_node = None
+    def _select_click(self, select_node):
+        if self.curr_node:
+            self.curr_node.piece.highlight(False)
+        self.curr_node = select_node
+        self.curr_node.piece.highlight(True)
+
+    def _move_click(self, move_node):
+        print(f"color : {self.curr_node.piece.color}\n"
+              f"curr_piece pos : {self.curr_node.position}\n"
+              f"valid moves : {self.curr_node.piece.valid_moves(self.curr_node)}\n"
+              f"valid moves pos : {[node.position for node in self.curr_node.piece.valid_moves(self.curr_node)]}")
+        if move_node in self.curr_node.piece.valid_moves(self.curr_node):
+            print(f"in valid moves {move_node}"
+                  f"piece? {move_node.piece}"
+                  f"move position {move_node.position}"
+                  f"curr position {self.curr_node.position}")
+            if move_node.piece:
+                move_node.piece.kill()
+            self.curr_node.piece.highlight(False)
+            self.curr_node.piece.update(move_node.position)
+            self.graph.update_nodes(start_node=self.curr_node, end_node=move_node)
+            self._change_player()
 
     def _get_node_from_click(self, click_pos):
         row = self._find_range_index(self.position.row_bounds, click_pos[1])
@@ -72,8 +96,19 @@ class Events:
 
     def _find_range_index(self, bounds, value):
         for i in range(1, len(bounds)):
-            if bounds[i - 1] < value < bounds[i]:
+            if bounds[i - 1] <= value <= bounds[i]:
                 return i - 1
+
+    def _type_click(self, node):
+        if not self.curr_node and (not node.piece or node.piece.color != self.curr_player):
+            return "null"
+        if not node.piece or node.piece.color != self.curr_player:
+            return "move"
+        return "select"
+
+    def _change_player(self):
+        self.curr_player = "white" if self.curr_player == "black" else "black"
+        self.curr_node = None
 
 
 class Node:
@@ -130,7 +165,7 @@ class Graph:
                         connected_node = self.node_matrix[indexes[0]][indexes[1]]
                         setattr(curr_node, direction, connected_node)
 
-    def update_node(self, start_node, end_node):
+    def update_nodes(self, start_node, end_node):
         piece = start_node.piece
         start_node.piece = None
         end_node.piece = piece
@@ -147,11 +182,31 @@ class Graph:
                 curr_node = curr_node.left
 
 
+class GameStatusBar:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.image = pygame.Surface((width, height))
+        self.rect = self.image.get_rect()
+        self.font = pygame.font.SysFont('Arial', 32)
+        self.update("White")
+
+    def update(self, player):
+        pygame.draw.rect(self.image, "black", (0, 0, self.width, self.height))
+        pygame.draw.rect(self.image, "white", (10, 10, self.width - 20, self.height - 20))
+        pos = self.rect.center
+        text = self.font.render(f"{player}'s Turn!", True, "Black")
+        pos = (pos[0] - text.get_rect().centerx, pos[1] - text.get_rect().centery)
+        self.image.blit(text, pos)
+
+
 class Piece(pygame.sprite.Sprite):
-    def __init__(self, color, width, height):
+    def __init__(self, x, y, color, width, height):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+        self.original_image = None  # initialize with value during construction of subclass
         self.rect = self.image.get_rect()
+        self.update((x, y))
         self.color = color
         self.move_history = []
         self.number_move = 0
@@ -159,28 +214,83 @@ class Piece(pygame.sprite.Sprite):
     def update(self, pos):
         self.rect.center = pos
 
-
-class Pawn(Piece):
-    def __init__(self, color, x, y, width, height):
-        super().__init__(color, width, height)
-        pygame.draw.rect(self.image, color, (0, 0, width, height))  # Fill
-        pygame.draw.rect(self.image, "red", (0, 0, width, height), 2)
-        self.update((x, y))
-
     def valid_moves(self, node):
         valid_moves_nodes = []
-        for node in self._check_diagonals(node):
-            valid_moves_nodes.append(node)
+        for n in self._check_diagonals(node):
+            valid_moves_nodes.append(n)
         valid_moves_nodes.append(self._check_forward_one(node))
         valid_moves_nodes.append(self._check_forward_two(node))
-        return valid_moves_nodes
+
+        return [n for n in valid_moves_nodes if n]
 
     def _check_diagonals(self, node):
         nodes = []
-        if (node.up_left and node.up_left.piece) and node.up_left.piece.color != self.color:
-            nodes.append(node.up_left)
-        if (node.up_right and node.up_right.piece) and node.up_right.piece.color != self.color:
-            nodes.append(node.up_right)
+        diag_l_node = getattr(node, self.move_color_codex[self.color]["dl"])
+        diag_r_node = getattr(node, self.move_color_codex[self.color]["dr"])
+
+        if (diag_l_node and diag_l_node.piece) and diag_l_node.piece.color != self.color:
+            nodes.append(diag_l_node)
+        if (diag_r_node and diag_r_node.piece) and diag_r_node.piece.color != self.color:
+            nodes.append(diag_r_node)
+        return nodes
+
+    def _check_forward_one(self, node):
+        forward_node = getattr(node, self.move_color_codex[self.color]["f"])
+        if not forward_node or forward_node.piece:
+            return
+        return forward_node
+
+    def highlight(self, active):
+
+        overlay = pygame.Surface(self.image.get_size(), pygame.SRCALPHA)
+        if active:
+            overlay.fill((128, 128, 128, 128))
+            self.image.blit(overlay, (0, 0))
+        else:
+            self.image = self.original_image.copy()
+
+
+class Pawn(Piece):
+    def __init__(self, x, y, color, width, height):
+        super().__init__(x, y, color, width, height)
+        image = pygame.image.load(f"./assets/imgs/pieces/chess_piece_2_{self.color}_pawn.png")
+        image = pygame.transform.scale(image, (width, height))
+        self.image.blit(image, (0, 0))
+        self.original_image = self.image.copy()
+
+        self.move_color_codex = {
+            "white":
+                {
+                    "f": "up",
+                    "dl": "up_left",
+                    "dr": "up_right"
+                },
+            "black":
+                {
+                    "f": "down",
+                    "dl": "down_left",
+                    "dr": "down_right",
+                }
+        }
+
+    def valid_moves(self, node):
+        valid_moves_nodes = []
+        for n in self._check_diagonals(node):
+            valid_moves_nodes.append(n)
+        valid_moves_nodes.append(self._check_forward_one(node))
+        valid_moves_nodes.append(self._check_forward_two(node))
+
+        return [n for n in valid_moves_nodes if n]
+
+    def _check_diagonals(self, node):
+        nodes = []
+        diag_l_node = getattr(node, self.move_color_codex[self.color]["dl"])
+        diag_r_node = getattr(node, self.move_color_codex[self.color]["dr"])
+
+        if (diag_l_node and diag_l_node.piece) and diag_l_node.piece.color != self.color:
+            nodes.append(diag_l_node)
+        if (diag_r_node and diag_r_node.piece) and diag_r_node.piece.color != self.color:
+            nodes.append(diag_r_node)
         return nodes
 
     def _check_forward_two(self, node):
@@ -189,12 +299,41 @@ class Pawn(Piece):
 
         curr_node = node
         for i in range(2):
-            if not curr_node.up or curr_node.up.piece:
+            forward_node = getattr(curr_node, self.move_color_codex[self.color]["f"])
+            if not forward_node or forward_node.piece:
                 return
-            curr_node = curr_node.up
+            curr_node = forward_node
         return curr_node
 
     def _check_forward_one(self, node):
-        if not node.up or node.up.piece:
+        forward_node = getattr(node, self.move_color_codex[self.color]["f"])
+        if not forward_node or forward_node.piece:
             return
-        return node.up
+        return forward_node
+
+
+class King(Piece):
+    def __init__(self, x, y, color, width, height):
+        super().__init__(color, width, height)
+        image = pygame.image.load(f"./assets/imgs/pieces/chess_piece_2_{self.color}_king.png")
+        image = pygame.transform.scale(image, (width, height))
+        self.image.blit(image, (0, 0))
+        self.original_image = self.image.copy()
+
+        self.move_color_codex = {
+            "white":
+                {
+                    "f": "up",
+                    "b": "down",
+                    "dl": "up_left",
+                    "dr": "up_right"
+                },
+            "black":
+                {
+                    "f": "down",
+                    "b": "up",
+                    "dl": "down_left",
+                    "dr": "down_right",
+                }
+        }
+    def valid_moves(self, node):
--- run.py
+++ run.py
@@ -1,38 +1,40 @@
 import pygame
-from classes import Graph, Position, Events, Pawn
+from classes import Graph, Position, Game, Pawn, GameStatusBar
 
-WIDTH = 800
-HEIGHT = 800
+CANVAS_WIDTH = 800
+CANVAS_HEIGHT = 950
 
-position = Position(WIDTH, HEIGHT)
+BOARD_WIDTH = 800
+BOARD_HEIGHT = 800
+
+START_X = CANVAS_WIDTH - BOARD_WIDTH
+START_Y = CANVAS_HEIGHT - BOARD_HEIGHT
+
+
+position = Position(START_X, START_Y, BOARD_WIDTH, BOARD_HEIGHT)
 graph = Graph(position.position_matrix)
-events = Events(position, graph)
+game = Game(position, graph)
 
 pygame.init()
 
 whites = pygame.sprite.Group()
 blacks = pygame.sprite.Group()
 
+game_status_bar = GameStatusBar(BOARD_WIDTH, START_Y)
+
 for i in range(8):
     pos = position.position_matrix[0][i]
-    new_pawn = Pawn("white", pos[0], pos[1], position.piece_width, position.piece_height)
-    whites.add(new_pawn)
+    new_pawn = Pawn("black", pos[0], pos[1], position.piece_width, position.piece_height)
+    blacks.add(new_pawn)
     graph.node_matrix[0][i].piece = new_pawn
 
 for i in range(8):
     pos = position.position_matrix[7][i]
-    new_pawn = Pawn("black", pos[0], pos[1], position.piece_width, position.piece_height)
-    blacks.add(new_pawn)
+    new_pawn = Pawn("white", pos[0], pos[1], position.piece_width, position.piece_height)
+    whites.add(new_pawn)
     graph.node_matrix[7][i].piece = new_pawn
 
-
-
-
-
-
-
-
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((CANVAS_WIDTH, CANVAS_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 
@@ -41,12 +43,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            events.handle_click(pygame.mouse.get_pos())
-    screen.fill("green")
+            game.handle_click(pygame.mouse.get_pos())
+            game_status_bar.update(game.curr_player)
 
+
+    screen.fill("green")
 
     whites.draw(screen)
     blacks.draw(screen)
+    screen.blit(game_status_bar.image, (0, 0))
     pygame.display.flip()
 
     clock.tick(60)
